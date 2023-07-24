ef6f47c5b535786385081a85aa36101e3b3a2eb6
Egnoel <70013986+Egnoel@users.noreply.github.com>
2023-07-24 15:28:31 +0100

    getting streamPath key

--- media-server.js
+++ media-server.js
@@ -15,18 +15,29 @@ const config = {
   },
   trans: {
     ffmpeg: "C:/ffmpeg/bin/ffmpeg.exe",
-    tasks: [
-      {
-        app: "live",
-        hls: true,
-        hlsFlags: "[hls_time=2:hls_list_size=3:hls_flags=delete_segments]",
-        dash: true,
-        dashFlags: "[f=dash:window_size=3:extra_window_size=5]",
-      },
-    ],
+    tasks: [],
   },
 };
 
 const nms = new NodeMediaServer(config);
+const createStream = (eventId) => ({
+  app: "live",
+  mode: "static",
+  edge: `rtmp://localhost/live/${eventId}`,
+  hls: true,
+  hlsFlags: "[hls_time=2:hls_list_size=3:hls_flags=delete_segments]",
+  dash: true,
+  dashFlags: "[f=dash:window_size=3:extra_window_size=5]",
+});
+nms.on("prePublish", (id, StreamPath, args) => {
+  // Verifique se o evento está começando com /live/
+  if (StreamPath.startsWith("/live/")) {
+    // Obtenha o ID do evento a partir do StreamPath
+    const eventId = StreamPath.slice(6); // Remove os primeiros 6 caracteres (/live/)
+
+    // Crie um novo fluxo com base no ID do evento e adicione-o às tarefas de transmissão
+    config.trans.tasks.push(createStream(eventId));
+  }
+});
 nms.run();
 export default nms;
